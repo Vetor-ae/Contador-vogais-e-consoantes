8e7106ddb73dd8e25472eee18abefe43c95cb230
Vitor Pires <vitorhp05@hotmail.com>
2025-10-15 14:21:45 -0300

    iniciando repositorio

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o que quiser(palavras, frases, sentencas,...):");
+        string frase = Console.ReadLine().ToLower();
+        char[] vogais = ['a', 'e', 'i', 'o', 'u'];
+        int vog = 0;
+        int cons = 0;
+        foreach (char caracter in frase)
+        {
+            for (int i = 0; i < vogais.Length; i++)
+            {
+                if (caracter == vogais[i])
+                {
+                    vog++;
+                }
+                else
+                {
+                    cons++;
+                } 
+            }
+        }
+        Console.WriteLine($"Qnt. de vogais = {vog}\nQnt. de consoantes = {cons}");
+    }
+}
